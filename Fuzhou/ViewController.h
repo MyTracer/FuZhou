8e31b1c0e9649da28ed2cf945961c0ec50518c6e
@J <TooT_cn@live.cn>
2016-05-28 11:25:04 +0800

    Initial Commit

---
+++ Fuzhou/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fuzhou
+//
+//  Created by 韩江鹏 on 16/5/28.
+//  Copyright © 2016年 韩江鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Fuzhou/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Fuzhou
+//
+//  Created by 韩江鹏 on 16/5/28.
+//  Copyright © 2016年 韩江鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
